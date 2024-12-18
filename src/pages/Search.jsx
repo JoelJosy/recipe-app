90593089b2663abd3d24c544a85a1934b1401572
Joel Josy <joeljosy79@gmail.com>
2024-12-18 19:01:45 +0530

    make loading/error screens

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import NavBar from '../components/NavBar'
-import Searchbar from '../components/Searchbar'
 import RecipeCard from '../components/RecipeCard'
 
 const recipes = [   {idMeal: 1, strMeal: "Chicken Biryani", strCategory: "Chicken", strMealThumb: "./", strArea: "Iranian"}, 
@@ -9,16 +8,60 @@ const recipes = [   {idMeal: 1, strMeal: "Chicken Biryani", strCategory: "Chicke
                   ];
 
 export default function Search() {
+  const [searchQuery, setSearchQuery] = useState("");
+  //const [recipes, setRecipes] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+
+  const handleSearch = () => {
+
+  };
+
+
+
   return (
     <>
       <NavBar />
-      <Searchbar />
+      <div className='search-bar my-12'>
+          <form onSubmit={handleSearch} className='flex items-center w-full max-w-md mx-auto'>
+            <input
+              type="text"
+              placeholder="Search..."
+              className="input input-bordered w-full shadow-sm"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type='submit' className="btn btn-primary ml-2 shadow-md">
+            <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </form>
+      </div>
 
-      <div className="recipes-grid flex flex-wrap justify-center gap-6 p-4">
+      {error && 
+      <div className="flex mt-64 items-center justify-center">
+        <h1 className="text-center text-5xl font-semibold tracking-tight text-gray-900">
+            Error, cannot load recipe...
+        </h1>
+      </div>}
+
+      {loading ? (
+        <div className="flex mt-64 items-center justify-center">
+          <div className="text-center flex items-center">
+            <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900">
+                Loading
+            </h1>
+            <div className="loading loading-dots loading-lg mt-7"></div>
+          </div>
+        </div>
+      ) : (
+        <div className="recipes-grid flex flex-wrap justify-center gap-6 p-4">
         {recipes.map((recipe) => (
           <RecipeCard recipe={recipe} key={recipe.idMeal}/>
         ))}
-      </div>
+        </div>
+      )}
+      
 
     </>
   )
